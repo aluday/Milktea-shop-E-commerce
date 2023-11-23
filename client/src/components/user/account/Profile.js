d9202551582a901f77d3f22920986c9ef66ae8dc
Van Luu <Van.Luu@iconplc.com>
2023-11-23 18:24:57 +0700

    Update stylings for User profile

--- client/src/components/user/account/Profile.js
+++ client/src/components/user/account/Profile.js
@@ -1,80 +1,60 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../../providers/UserProvider";
 import Header from "../../shared-components/Header";
-import { Flex, Row, Col, Avatar, Space, List } from "antd";
-import { UserOutlined } from "@ant-design/icons";
-
-const data = [
-  {
-    title: "Ant Design Title 1",
-  },
-  {
-    title: "Ant Design Title 2",
-  },
-  {
-    title: "Ant Design Title 3",
-  },
-  {
-    title: "Ant Design Title 4",
-  },
-];
+import { Flex, Avatar, Card, Row, Col, Divider, Space, Empty } from "antd";
+import { UserOutlined, EditOutlined } from "@ant-design/icons";
 
 export const Profile = () => {
   const { currentUser } = useContext(UserContext);
 
   return (
-    <div>
+    <>
       <Header currentUser={currentUser} />
       <Flex className="userProfileContainer" align="center" justify="center">
-        <div className="userInfo">
-          <div className="userAvt">
-            <Space>
-              <Avatar icon={<UserOutlined />} />
-            </Space>
-          </div>
-          <Row className="info" align="center" justify="space-between">
-            <Col span={6}>
-              <List
-                itemLayout="horizontal"
-                dataSource={data}
-                renderItem={(item, index) => (
-                  <List.Item>
-                    <List.Item.Meta
-                      avatar={
-                        <Avatar
-                          src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${index}`}
-                        />
-                      }
-                      title={<a href="https://ant.design">{item.title}</a>}
-                      description=""
-                    />
-                  </List.Item>
-                )}
-              />
+        <div className="userInfoWrapper">
+          <Flex vertical align="center" justify="center">
+            <Avatar size={72} icon={<UserOutlined />} />
+            <h1>Anh Luu</h1>
+            <p className="text-link"> Chỉnh sửa thông tin </p>
+          </Flex>
+          <Divider />
+          <Row gutter={16}>
+            <Col span={8}>
+              <Card className="basicInfoCardContainer" title="Thông tin cơ bản" bordered={false}>
+                <Flex className="basicInfoContainer" vertical justify="center">
+                  <Flex justify="space-between">
+                    <Space><h4>Họ tên:</h4></Space>
+                    <Space><p>Anh Luu</p></Space>
+                  </Flex>
+                  <Flex justify="space-between">
+                    <Space><h4>Số điện thoại:</h4></Space>
+                    <Space><p>0123456789</p></Space>
+                  </Flex>
+                  <Flex justify="space-between">
+                    <Space><h4>Email:</h4></Space>
+                    <Space><p>abc@gmail.com</p></Space>
+                  </Flex>
+                  <Flex justify="space-between">
+                    <Space><h4>Địa chỉ:</h4></Space>
+                    <Space><p>abc@gmail.com</p></Space>
+                  </Flex>
+                </Flex>
+              </Card>
             </Col>
-            <Col span={6}>
-              <List
-                itemLayout="horizontal"
-                dataSource={data}
-                renderItem={(item, index) => (
-                  <List.Item>
-                    <List.Item.Meta
-                      avatar={
-                        <Avatar
-                          src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${index}`}
-                        />
-                      }
-                      title={<a href="https://ant.design">{item.title}</a>}
-                      description=""
-                    />
-                  </List.Item>
-                )}
-              />
+            <Col span={8}>
+              <Card title="Thông tin bổ sung" bordered={false}>
+                <Empty />
+              </Card>
+            </Col>
+            <Col span={8}>
+              <Card title="Thiết lập hệ thống" bordered={false}>
+                <Empty />
+              </Card>
             </Col>
           </Row>
         </div>
       </Flex>
-    </div>
+    </>
   );
 };
 
--- client/src/components/user/payment/PaymentDetails.js
+++ client/src/components/user/payment/PaymentDetails.js
@@ -1,27 +1,25 @@
 import React, { useState, useContext } from "react";
-import { UserContext } from "../../../providers/UserProvider";
-import { OrderContext } from "../../../providers/OrderProvider";
+// import { UserContext } from "../../../providers/UserProvider";
+// import { OrderContext } from "../../../providers/OrderProvider";
 import Header from "../../shared-components/Header";
-import { Button, message, Steps, theme, Flex } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import { Flex } from "antd";
 import PaymentSteppers from "./PaymentSteppers";
 import OrderList from "./OrderList";
 import UserInfo from "./UserInfo";
 import "./PaymentDetails.css";
 
-const steps = [
-  {
-    title: 'Thông tin đơn hàng',
-    content: <OrderList />,
-  },
-  {
-    title: 'Đặt hàng',
-    content: <UserInfo />,
-  }
-];
-
 export const PaymentDetails = () => {
   const [currentStep, setCurrentStep] = useState(0);
+  const steps = [
+    {
+      title: 'Thông tin đơn hàng',
+      content: <OrderList />,
+    },
+    {
+      title: 'Đặt hàng',
+      content: <UserInfo />,
+    }
+  ];
 
   const nextStep = () => {
     setCurrentStep(currentStep + 1);
